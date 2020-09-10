aa8b18cdcc0e9de8137ed6e6ed2818275fd6d1a8
MattWotus <mawotus@gmail.com>
2020-09-10 19:14:26 +0000

    Completed view-average-grade feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,6 +7,7 @@ class App extends React.Component {
     super(props);
     this.state = { grades: [] };
     this.getAllGrades = this.getAllGrades.bind(this);
+    this.getAverageGrade = this.getAverageGrade.bind(this);
   }
 
   componentDidMount() {
@@ -19,13 +20,27 @@ class App extends React.Component {
       .then(data => this.setState({ grades: data }));
   }
 
+  getAverageGrade() {
+    let total = 0;
+    if (this.state.grades.length === 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < this.state.grades.length; i++) {
+        total = total + this.state.grades[i].grade;
+      }
+      const average = total / this.state.grades.length;
+      return average;
+    }
+  }
+
   render() {
-    if (this.state.grades === []) {
+    const calculatedAverage = this.getAverageGrade();
+    if (this.state.grades.length === 0) {
       return <h3>No grades Recorded</h3>;
     } else {
       return (
         <div className="container-fluid">
-          <Header text="Student Grade Table" />
+          <Header text="Student Grade Table" average={calculatedAverage} />
           <GradeTable students={this.state.grades} />
         </div>
       );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <h1 className="mt-3 mb-3">{ props.text }</h1>
+    <div className="mt-3 mb-3 d-flex justify-content-between align-items-center">
+      <h1>{props.text}</h1>
+      <h2>Average: <span className="badge badge-secondary">{props.average}</span></h2>
+    </div>
   );
 }
 
