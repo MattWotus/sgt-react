874b23afc4521c1af975e87486ae8a22e1263666
MattWotus <mawotus@gmail.com>
2020-10-13 04:43:05 +0000

    In progress fetch put request

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -6,6 +6,7 @@ class GradeForm extends React.Component {
     this.state = { name: '', course: '', grade: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
     this.handleReset = this.handleReset.bind(this);
   }
 
@@ -26,6 +27,17 @@ class GradeForm extends React.Component {
     this.handleReset();
   }
 
+  handleUpdate(event) {
+    event.preventDefault();
+    const newGrade = {
+      name: this.state.name,
+      course: this.state.course,
+      grade: this.state.grade
+    };
+    this.props.updateGrade(newGrade);
+    this.handleReset();
+  }
+
   handleReset() {
     this.setState({ name: '', course: '', grade: '' });
     event.target.reset();
@@ -37,37 +49,68 @@ class GradeForm extends React.Component {
       width: '20px',
       height: '16px'
     };
-    return (
-      <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1"><i className="fas fa-user" style={iconStyle}></i></span>
+    if (this.props.singleGrade.name === '') {
+      return (
+        <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1"><i className="fas fa-user" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Name" name="name" aria-label="name"
+              aria-describedby="basic-addon1" defaultValue={this.props.singleGrade.name} onChange={this.handleChange} />
           </div>
-          <input type="text" className="form-control" placeholder="Name" name="name" aria-label="name"
-            aria-describedby="basic-addon1" defaultValue={this.props.singleGrade.name} onChange={this.handleChange} />
-        </div>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon2"><i className="fas fa-chalkboard" style={iconStyle}></i></span>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon2"><i className="fas fa-chalkboard" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Course" name="course" aria-label="course"
+              aria-describedby="basic-addon2" defaultValue={this.props.singleGrade.course} onChange={this.handleChange} />
           </div>
-          <input type="text" className="form-control" placeholder="Course" name="course" aria-label="course"
-            aria-describedby="basic-addon2" defaultValue={this.props.singleGrade.course} onChange={this.handleChange} />
-        </div>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon3"><i className="fas fa-graduation-cap" style={iconStyle}></i></span>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon3"><i className="fas fa-graduation-cap" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Grade" name="grade" aria-label="grade"
+              aria-describedby="basic-addon3" defaultValue={this.props.singleGrade.grade} onChange={this.handleChange} />
           </div>
-          <input type="text" className="form-control" placeholder="Grade" name="grade" aria-label="grade"
-            aria-describedby="basic-addon3" defaultValue={this.props.singleGrade.grade} onChange={this.handleChange} />
-        </div>
-        <div>
-          <button id="addButton" type="submit" className="btn btn-success pl-3 pr-3 mr-2">Add</button>
-          <button type="reset" className="btn btn-outline-secondary pl-3 pr-3">Cancel</button>
-        </div>
-      </form>
-    );
+          <div>
+            <button id="addButton" type="submit" className="btn btn-success pl-3 pr-3 mr-2">Add</button>
+            <button type="reset" className="btn btn-outline-secondary pl-3 pr-3">Cancel</button>
+          </div>
+        </form>
+      );
+    } else {
+      return (
+        <form onSubmit={this.handleUpdate} onReset={this.handleReset}>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1"><i className="fas fa-user" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Name" name="name" aria-label="name"
+              aria-describedby="basic-addon1" defaultValue={this.props.singleGrade.name} onChange={this.handleChange} />
+          </div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon2"><i className="fas fa-chalkboard" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Course" name="course" aria-label="course"
+              aria-describedby="basic-addon2" defaultValue={this.props.singleGrade.course} onChange={this.handleChange} />
+          </div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon3"><i className="fas fa-graduation-cap" style={iconStyle}></i></span>
+            </div>
+            <input type="text" className="form-control" placeholder="Grade" name="grade" aria-label="grade"
+              aria-describedby="basic-addon3" defaultValue={this.props.singleGrade.grade} onChange={this.handleChange} />
+          </div>
+          <div>
+            <button id="addButton" type="submit" className="btn btn-success pl-3 pr-3 mr-2">Update</button>
+            <button type="reset" className="btn btn-outline-secondary pl-3 pr-3">Cancel</button>
+          </div>
+        </form>
+      );
+    }
   }
-
 }
 
 export default GradeForm;
